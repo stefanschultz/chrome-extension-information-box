6c5670e310fe0053bd25a9491cb650c478e482c7
Stefan Schultz <info@schultzsoftware.com>
2023-08-08 13:40:22 +0200

    Mouse btn

--- src/pages/contentScript/components/MouseMoveAndPosition.tsx
+++ src/pages/contentScript/components/MouseMoveAndPosition.tsx
@@ -3,16 +3,36 @@ import "../styles/InformationBox.styles.css";
 
 const MouseMoveAndPosition = () => {
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    const [mouseButton, setMouseButton] = useState("");
 
     const handleMouseMove = (event: MouseEvent) => {
         setMousePosition({ x: event.clientX, y: event.clientY });
     };
 
+    const handleClick = (event: MouseEvent) => {
+        switch (event.button) {
+            case 0:
+                setMouseButton("Left");
+                break;
+            case 1:
+                setMouseButton("Middle");
+                break;
+            case 2:
+                setMouseButton("Right");
+                break;
+            default:
+                setMouseButton("");
+                break;
+        }
+    };
+
     useEffect(() => {
         document.body.addEventListener("mousemove", handleMouseMove);
+        document.body.addEventListener("click", handleClick);
 
         return () => {
             document.body.removeEventListener("mousemove", handleMouseMove);
+            document.body.removeEventListener("click", handleClick);
         };
     }, []);
 
@@ -20,6 +40,7 @@ const MouseMoveAndPosition = () => {
         <div className="panel-container-mousemoveandposition">
             <div>X: {mousePosition.x}</div>
             <div>Y: {mousePosition.y}</div>
+            <div>Button: {mouseButton}</div>
         </div>
     );
 };
